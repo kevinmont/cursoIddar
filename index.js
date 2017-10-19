782bc4681a467e6d1627ca55f8312bad45b51bfa
THEZCKBOY\Jonathan <serrano@blackflag.mx>
2017-10-19 18:35:22 -0500

    Primer Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// const lodash = require('lodash')
+
+const obj = {
+  nombre: 'iddar',
+  edad: 29,
+  email: 'iddar@dbug.mx',
+  instagram: '@thezckboy6'
+}
+
+console.log(obj.nombre)
+console.log(obj.instagram)
+
+//
+// function hola () {
+//   return 'hola mundo x2'
+// }
+//
+// console.log('hola mundo')
+// console.log(hola())
+// console.log(2 + 2)
+//
+// var lista = [1, 2, 3, 4, 5]
+// console.log(lista)
